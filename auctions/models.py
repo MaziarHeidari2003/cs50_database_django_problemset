9f94a5f1964a652116179a0bfb758e0e28d00c7c
MaziarHeidari2003 <maziarheidari1124@gmail.com>
2024-01-29 05:48:37 -0800

    is git wodking fine? lets check it out

--- auctions/models.py
+++ auctions/models.py
@@ -1,25 +1,17 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
-
 class User(AbstractUser):
-    pass
+  pass
+
 
 class Category(models.Model):
-    category_name = models.CharField(max_length=50)
-    def __str__(self):
-        return self.category_name
+  category_name = models.CharField(max_length=50)
 
 class Listing(models.Model):
-    title = models.CharField(max_length=30)
-    description = models.CharField(max_length=300)
-    image_url = models.CharField(max_length=1000)
-    price = models.FloatField()
-    is_active = models.BooleanField(default=True)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name="user")
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True,blank=True, related_name="category")
-    watch_list = models.ManyToManyField(User,blank=True, related_name="listingwatchlist")
-
-    def __str__(self):
-        return self.title
+  title = models.CharField(max_length=30)
+  description = models.CharField(max_length=300)
+  image_url = models.CharField(max_length=3000)
+  is_active = models.BooleanField(default=True)
+  owner = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name='user')
+  category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True, related_name="category" )
